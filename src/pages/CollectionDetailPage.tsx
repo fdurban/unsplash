e80b08d5ba2600adaf92f6c8732b9c2e75919e4a
Fernando Durban <ferbrizie@gmail.com>
2024-03-19 14:02:09 +0100

    tailwind prettier

--- src/pages/CollectionDetailPage.tsx
+++ src/pages/CollectionDetailPage.tsx
@@ -7,9 +7,16 @@ interface Photos {
 	urls: {
 		raw: string
 	}
+	current_user_collections: [
+		{
+			title: string
+		},
+	]
 }
 
 const CollectionDetailPage: React.FC = () => {
+	const [collectionTitle, setCollectionTitle] = useState<string>('')
+	const [collectionNumber, setCollectionNumber] = useState<string>('')
 	const [photos, setPhotos] = useState<Photos[]>([])
 	const { id } = useParams<{ id: string }>()
 
@@ -18,6 +25,12 @@ const CollectionDetailPage: React.FC = () => {
 			try {
 				const result = await photoHandler.getCollectionsPhotos(id!)
 				setPhotos(result)
+				if (result.length > 0) {
+					setCollectionTitle(result[0].current_user_collections[0].title)
+					setCollectionNumber(
+						result[0].current_user_collections[0].total_photos,
+					)
+				}
 			} catch (error) {
 				console.error('Error fetching photos:', error)
 			}
@@ -25,16 +38,25 @@ const CollectionDetailPage: React.FC = () => {
 
 		fetchData()
 	}, [id])
-	console.log(photos)
 
 	return (
 		<>
-			{photos.map(photo => (
-				<>
-					<img src={photo.urls.raw} alt='' />
-				</>
-			))}
-			<h1>This is the Collections Photos</h1>
+			<div className='flex flex-col mt-5 items-center justify-center'>
+				<h1 className='bg-gradient-to-r from-yellow-200 to-pink-900 inline-block text-transparent bg-clip-text text-5xl font-semibold'>
+					{collectionTitle}
+				</h1>
+				<h1 className='mt-2 font-semibold'>{collectionNumber} photos</h1>
+			</div>
+			<div className='flex flex-wrap justify-center my-8 px-36'>
+				{photos.map((photo, index) => (
+					<img
+						key={index}
+						src={photo.urls.raw}
+						alt=''
+						className='max-w-xs m-2 object-fill rounded-md'
+					/>
+				))}
+			</div>
 		</>
 	)
 }
--- src/pages/PhotoPage.tsx
+++ src/pages/PhotoPage.tsx
@@ -2,81 +2,91 @@ import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import photoHandler from '../services/unsplash.services'
 
-
 interface UnsplashResult {
-  id: string
-  urls: {
-    regular: string
-  }
+	id: string
+	urls: {
+		regular: string
+	}
 }
 
 interface UnsplashResponse {
-  results: UnsplashResult[]
+	results: UnsplashResult[]
 }
 
 const PhotoPage: React.FC = () => {
-  const { query: urlQuery } = useParams<{ query: string }>()
-  const navigate = useNavigate()
+	const { query: urlQuery } = useParams<{ query: string }>()
+	const navigate = useNavigate()
 
-  const [searchQuery, setSearchQuery] = useState<string>('')
-  const [foundData, setFoundData] = useState<UnsplashResponse>({ results: [] })
+	const [searchQuery, setSearchQuery] = useState<string>('')
+	const [foundData, setFoundData] = useState<UnsplashResponse>({ results: [] })
 
-  const handleSearch = async (query: string): Promise<void> => {
-    try {
-      const data = await photoHandler.searchOnUnsplash(query)
-      setFoundData(data)
-      navigate(`/query/${query}`)
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      setFoundData({ results: [] })
-    }
-  }
+	const handleSearch = async (query: string): Promise<void> => {
+		try {
+			const data = await photoHandler.searchOnUnsplash(query)
+			setFoundData(data)
+			navigate(`/query/${query}`)
+		} catch (error) {
+			console.error('Error fetching data:', error)
+			setFoundData({ results: [] })
+		}
+	}
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    e.preventDefault()
-    await handleSearch(searchQuery)
-  }
+	const handleSubmit = async (
+		e: React.FormEvent<HTMLFormElement>,
+	): Promise<void> => {
+		e.preventDefault()
+		await handleSearch(searchQuery)
+	}
 
-  const handleRedirect = async (photoId: string): Promise<void> => {
-    try {
-      navigate(`/photoid/${photoId}`)
-    } catch (error) {
-      console.error('Error redirecting:', error)
-    }
-  }
+	const handleRedirect = async (photoId: string): Promise<void> => {
+		try {
+			navigate(`/photoid/${photoId}`)
+		} catch (error) {
+			console.error('Error redirecting:', error)
+		}
+	}
 
-  useEffect(() => {
-    if (urlQuery) {
-      handleSearch(urlQuery)
-    }
-  }, [urlQuery])
+	useEffect(() => {
+		if (urlQuery) {
+			handleSearch(urlQuery)
+		}
+	}, [urlQuery])
 
-  return (
-    <div className="relative">
-      <img src="../../public/assets/gradiend-bg.svg" alt="bg-different-colors" className="w-screen" />
-      <div className="absolute top-2 left-0 right-0 flex justify-center">
-        <form onSubmit={handleSubmit} className="bg-white shadow-lg rounded-lg flex items-center">
-          <input onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search beautiful images" className=" border-none focus:outline-none ml-4" />
-          <button className="pt-4"><img src="../../public/assets/Search.svg" alt="" className="pb-4" /></button>
-        </form>
-      </div>
-      <div className='flex flex-wrap justify-center my-12 px-36'>
-        {foundData.results.map((result, index) => (
-          <img
-            key={index}
-            src={result.urls.regular}
-            alt={`Image ${index}`}
-            className="max-w-xs m-2 object-fill rounded"
-            onClick={() => handleRedirect(result.id)}
-          />
-        ))}
-      </div>
-    </div>
-  )
-
-
-
+	return (
+		<div className='relative'>
+			<img
+				src='../../public/assets/gradiend-bg.svg'
+				alt='bg-different-colors'
+				className='w-screen'
+			/>
+			<div className='absolute top-2 left-0 right-0 flex justify-center'>
+				<form
+					onSubmit={handleSubmit}
+					className='bg-white shadow-lg rounded-lg flex items-center'
+				>
+					<input
+						onChange={e => setSearchQuery(e.target.value)}
+						placeholder='Search beautiful images'
+						className=' border-none focus:outline-none ml-4'
+					/>
+					<button className='pt-4'>
+						<img src='../../public/assets/Search.svg' alt='' className='pb-4' />
+					</button>
+				</form>
+			</div>
+			<div className='flex flex-wrap justify-center my-12 px-36'>
+				{foundData.results.map((result, index) => (
+					<img
+						key={index}
+						src={result.urls.regular}
+						alt={`Image ${index}`}
+						className='max-w-xs m-2 object-fill rounded'
+						onClick={() => handleRedirect(result.id)}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default PhotoPage
-
