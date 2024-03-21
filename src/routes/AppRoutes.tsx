4d8c07e2261a1776a7e7c26aa6a2fe41734dfad5
Fernando Durban <ferbrizie@gmail.com>
2024-03-21 13:35:22 +0100

    done

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,23 +1,11 @@
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import PhotoPage from '../pages/PhotoPage'
 import { PhotoDetailPage } from '../pages/PhotoDetailPage'
 import CollectionsPage from '../pages/CollectionsPage'
 import CollectionDetailPage from '../pages/CollectionDetailPage'
-import React from 'react'
 
 const AppRoutes = () => {
-	const navigate = useNavigate()
-
-	React.useEffect(() => {
-		const params = new URLSearchParams(window.location.search)
-		const code = params.get('code')
-
-		if (code) {
-			navigate(`/collections?code=${code}`)
-			console.log('Received code:', code)
-		}
-	}, [])
 	return (
 		<Routes>
 			<Route path='/' element={<HomePage />} />
@@ -25,7 +13,6 @@ const AppRoutes = () => {
 			<Route path='/photoid/:id' element={<PhotoDetailPage />} />
 			<Route path='/collections' element={<CollectionsPage />} />
 			<Route path='/collection/:id' element={<CollectionDetailPage />} />
-			<Route path='/' />
 			<Route path='*' element={'404'} />
 		</Routes>
 	)
